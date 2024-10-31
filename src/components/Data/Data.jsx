935a9525a96058394059de7d26ca9729ef0e096e
Ehmkayel <lamidimorufat0@gmail.com>
2024-10-31 02:25:33 +0100

    feat: created the gallery, moment, and a data file

---
+++ src/components/Data/Data.jsx
@@ -0,0 +1,41 @@
+import { FaCalendar, FaCameraRetro, FaFile, FaUser } from "react-icons/fa"
+import { assets } from '../../assets/assets'
+
+
+export const aboutFeatures = [
+  {
+    id: 1,
+    title: 'Easily find nearby child-friendly spots!',
+    icon: <FaFile color="blue"  />,
+  },
+  {
+    id: 2,
+    title: 'Stay updated on fun activities and special events',
+    icon: <FaCalendar color="blue" />,
+  },
+  {
+    id: 3,
+    title: 'Check out images of each location in our Photo Galleries',
+    icon: <FaCameraRetro color="blue" />,
+  },
+  {
+    id: 4,
+    title: 'Get real insights from other parents through User Reviews',
+    icon: <FaUser color="blue"  />,
+  },
+
+];
+
+export const images = [
+  assets.entertainment,
+  assets.gallery,
+  assets.family,
+  assets.maq,
+  assets.view,
+  assets.playground,
+  assets.fun,
+  assets.moment,
+  assets.swift,
+  assets.blossom,
+  assets.hero,
+];
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <main className="bg-primary px-4 lg:px-[3rem] xl:px-[5rem] pt-[5rem] md:pt-0">
       <section className="flex flex-col md:flex-row h-[700px] md:h-[600px] xl:h-[700px] items-center container justify-between max-w-[1440px] w-full mx-auto">
         <figcaption className="text-white w-full md:w-[50%] text-center md:text-start">
-          <h1 className="text-3xl md:text-4xl xl:text-6xl font-bold mb-4"> Children favourites entertainment Centers</h1>
+          <h1 className="text-2xl md:text-4xl font-bold mb-4 heading"> Children favourites entertainment Centers</h1>
           <p className="text-lg mb-8">Find safe and engaging spaces for your kids, compare child-friendly centers, playgrounds, and resorts, making it simple for parents to choose the best adventures for their little ones and also to connect with other parents. </p>
           <span className="relative inline-flex items-center">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75" />
@@ -19,6 +19,7 @@ const Hero = () => {
         <figure
           className="w-64 h-64 md:w-80 md:h-80 xl:w-[500px] xl:h-[500px] rounded-lg md:rounded-full overflow-hidden flex-shrink-0 bg-cover bg-center transform transition duration-500 hover:scale-105"
           style={{ backgroundImage: `url(${assets.hero})` }}
+          alt="Kids playing games"
         />
       </section>
     </main>
